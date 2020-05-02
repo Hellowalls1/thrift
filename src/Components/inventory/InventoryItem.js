cc77f21057ea9d85d1cab1f3db82e26268f13f04
Hellowalls1 <thesingingbutcher@gmail.com>
2020-05-02 15:32:30 -0500

    got the location of where the item was purchased to display

--- src/Components/inventory/InventoryItem.js
+++ src/Components/inventory/InventoryItem.js
@@ -1,14 +1,14 @@
 import React from "react"
 
-export default ({ inventoryItem, type }) => (
+export default ({ inventoryItem, type, location }) => (
     <section className="inventoryItem">
 
         <h3 className="inventory__name">{inventoryItem.name}</h3>
-        <div className="inventory__type">{type.type}</div>
-        <div className="inventory__location">{inventoryItem.locationId}</div>
+        <div className="inventory__type">Item Type: {type.type}</div>
+        <div className="inventory__location">Purchased @: {location.name}</div>
         <div className="inventory__description">Description: {inventoryItem.description}</div>
         <div className="inventory__purchasePrice">Purchase Price: {inventoryItem.purchasePrice}</div>
-        <div className="inventory__forSale">For Sale?: {inventoryItem.forSale}</div>
+        <div className="inventory__forSale">For Sale ? : {inventoryItem.forSale}</div>
         <div className="inventory__salePrice">Sale Price: {inventoryItem.salePrice}</div>
         <div className="inventory__timePurchased">Time Purchased: {inventoryItem.time}</div>
     </section>
--- src/Components/inventory/InventoryList.js
+++ src/Components/inventory/InventoryList.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react"
 import { InventoryItemContext } from "./InventoryProvider" //importing context object from Provider
 import { ItemTypeContext } from "./ItemTypeProvider"
+import { StoreLocationContext } from "../location/StoreLocationProvider"
 import { Button} from "reactstrap"
 import InventoryItem from "./InventoryItem"
 
@@ -12,6 +13,7 @@ export default () => {
     // const  { users } = useContext(UserContext)
     const { inventoryItems } = useContext(InventoryItemContext)
     const { itemTypes } = useContext(ItemTypeContext)
+    const { storeLocations } =useContext(StoreLocationContext)
     const [modal, setModal] = useState(false)
     const toggle = () => setModal(!modal)
 
@@ -27,9 +29,12 @@ export default () => {
         {
                inventoryItems.map(inv => {
                   const matchingItemType = itemTypes.find(type => type.id === inv.itemTypeId)
-               
+                  const matchingStoreLocation = storeLocations.find(store => store.id === inv.locationId
+                    
+                    )
               return <InventoryItem key={inv.id}
               type={matchingItemType}
+              location={matchingStoreLocation}
               inventoryItem={inv} />
                 })
         }) 
